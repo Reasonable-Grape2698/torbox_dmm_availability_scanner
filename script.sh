5af1934e5b0f4f4173202b931c7dba995923e402
Reasonable-Grape2698 <dyl.secure@gmail.com>
2025-01-19 05:20:03 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,14 +11,18 @@ do
     esac
 done
 
-# Format into lines of 150 hashes per call, seperated by commas (curl has a maximum line length, this avoids it)
+# Format into lines of 150 hashes per call (curl maximum)
 hashes=$(jq '.[].hash' $input)
 tempfile=$(mktemp)
 echo $hashes | tr ' ' ',' | tr -d '"' | fold -w 6150 >> $tempfile
+percentile=100
 
+tasks_in_total=$(echo $hashes | tr ' ' ',' | tr -d '"' | fold -w 6150 | wc -l)
+count=0
 # For each line of 150 hashes, curl API
-# Format jq.data, too entries[], select key(s) (hashes)
-while IFS= read -r line; do
+# Format jq.data, too entries[], select key(s)(hashes)
+# Format and output as single lines to output.txt
+while read line; do
         temp=$(curl -s -H "Authorization: Bearer $apitoken" https://api.torbox.app/v1/api/torrents/checkcached?hash=$line&format=object&list_files=true)
         temp=$(echo $temp | jq .data)
         # If data isn't empty, select key (hash) and output as single lines to output.txt
@@ -28,6 +32,9 @@ while IFS= read -r line; do
                 echo $temp | tr -d ' ' | fold -w 40 >> $output
                 echo $temp | tr -d ' ' | fold -w 40
         fi
-done < $tempfile
+        count=$(echo $count+1 | bc)
+        percent="$(echo "$count" "$tasks_in_total" |awk '{printf "%.2f", $1 * 100 / $2}')"
+        echo "$count / $tasks_in_total $percent %"
+done < <(cat $tempfile)
 
 rm $tempfile
